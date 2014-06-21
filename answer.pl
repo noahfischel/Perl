77bec7f42de249887f56d0993560eb7b2155fe6d
Noah Fischel <Noah@Noah-Fischels-MacBook.local>
2014-06-21 16:35:15 -0700

    Added the answer to the bucket problem.

---
+++ answer.pl
@@ -0,0 +1,6 @@
+  #!/usr/bin/perl
+  print "There is no solution to the 86, 75, 309: 31, 41, 59 Problem";
+
+#Working it out on paper, I have found that there is no solution
+#to the problem. It would take magic to solve it using the legal
+#moves given. And no one can program magic… Yet.
